74329af6f794b82d1ec00e537485f10b6fff4f5a
a-ryang <chnwu.kim@gmail.com>
2023-07-24 18:15:35 -0700

    섹션 1: 쿼리 생성 및 로딩/에러 상태
    * staleTime 데이터가 만료됐다고 판단하기 전까지 허용하는 시간
    * 이 전까지 refretch는 이뤄지지 않는다

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -14,8 +15,17 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {
+    staleTime: 2000,
+  });
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Ooops, something went wrong</h3>
+        <p>{error.message}</p>
+      </>
+    );
 
   return (
     <>
