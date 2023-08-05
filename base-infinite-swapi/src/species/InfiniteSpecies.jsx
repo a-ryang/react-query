caf581f49ed237744933fece6c9bd385e51a3234
a-ryang <chnwu.kim@gmail.com>
2023-08-05 10:52:18 +0900

    섹션 3: infinite-scroll

---
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -0,0 +1,51 @@
+import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
+
+import { Species } from "./Species";
+
+const initialUrl = "https://swapi.dev/api/species/";
+const fetchUrl = async (url) => {
+  const response = await fetch(url);
+  return response.json();
+};
+
+export function InfiniteSpecies() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isLoading,
+    isError,
+    error,
+  } = useInfiniteQuery(
+    ["sw-species"],
+    ({ pageParm = initialUrl }) => fetchUrl(pageParm),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || null,
+    }
+  );
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error! {error.toString()}</div>;
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((species) => {
+            return (
+              <Species
+                key={species.name}
+                name={species.name}
+                language={species.language}
+                averageLifespan={species.average_lifespan}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
+}
